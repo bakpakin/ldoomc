bad19424fb30f23fdbc3cd55c0fb49088137e751
bakpakin <calsrose@gmail.com>
2015-12-30 09:46:37 -0500

    Hopefully allow ldoom to build on other platforms besides Mac.

--- src/config.h
+++ src/config.h
@@ -2,8 +2,11 @@
 #define CONFIG_HEADER
 
 #ifdef __APPLE__
-#define OPENGL_H <OpenGL/gl3.h>
-#define SDL_H <SDL2/SDL.h>
+    #define OPENGL_H <OpenGL/gl3.h>
+#else
+    #define OPENGL_H <GL/gl3.h>
 #endif
+
+#define SDL_H <SDL2/SDL.h>
 
 #endif
