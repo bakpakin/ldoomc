aa833ece525e89dd5b2d0eb5d6b8bcfd36a4dbe8
Calvin Rose <calsrose@gmail.com>
2016-03-13 15:42:27 -0400

    Fack.

--- src/glfw.h
+++ src/glfw.h
@@ -1,11 +1,7 @@
 #ifndef GLFW_LOCAL_HEADER
 #define GLFW_LOCAL_HEADER
 
-#ifdef __APPLE__
-#define GLFW_INCLUDE_GLCOREARB
-#endif
-
-#define GLFW_INCLUDE_GL_3
+#define GLFW_INCLUDE_GLCOREARB 1
 #include "GLFW/glfw3.h"
 
 #endif
