b076c58a9df495c1a88343bfe0d2357e3e9e7443
bakpakin <calsrose@gmail.com>
2016-05-20 14:04:01 +0800

    Fix audio.h header to get OpenAL on OSX.

--- src/audio.h
+++ src/audio.h
@@ -2,8 +2,13 @@
 #define AUDIO_H_WMDLVZMG
 
 #include <stdint.h>
+#ifdef __APPLE__
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#else
 #include <AL/al.h>
 #include <AL/alc.h>
+#endif
 #include "stb_vorbis.h"
 #include "ldmath.h"
 
